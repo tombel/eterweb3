fae0eb25161d0dced0b5c24891e5700ce8958aad
tombel <tomasbelardo@gmail.com>
2023-05-22 12:43:42 -0300

    correcciones de estilo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,74 @@
 import React, { useState } from 'react';
-import logo from "../assets/logo.svg"
-import Home from "./Home"
-import About from "./About"
-import Work from "./Work"
-import Contact from "./Contact"
-import Projects from "./Projects"
-import Press from "./Press"
+import logo from "../assets/logo.svg";
+import Home from "./Home";
+import About from "./About";
+import Work from "./Work";
+import Contact from "./Contact";
+import Projects from "./Projects";
+import Press from "./Press";
 
 function Navbar() {
-    const [activeSection, setActiveSection] = useState(0);
+  const [activeSection, setActiveSection] = useState(0);
 
-    const handleSectionClick = (index) => {
-        setActiveSection(index);
+  const handleSectionClick = (index) => {
+    setActiveSection(index);
+
+    // Change the background image based on the section clicked
+    switch (index) {
+      case -1:
+        document.body.style.backgroundImage = "url('/src/assets/images/fondo4.png')";
+        break;
+      case 0:
+        document.body.style.backgroundImage = "url('/src/assets/images/lab.png')";
+        break;
+      case 1:
+        document.body.style.backgroundImage = "url('/src/assets/images/fondo5.png')";
+        break;
+      case 2:
+        document.body.style.backgroundImage = "url('/src/assets/images/fondo2.png')";
+        break;
+      case 3:
+        document.body.style.backgroundImage = "url('/src/assets/images/lab.png')";
+        break;
+      case 4:
+        document.body.style.backgroundImage = "url('/src/assets/images/fondo6.png')";
+        break;
+      default:
+        // Handle the default case or do nothing
+        break;
     }
+    document.body.style.backgroundSize = "cover";
 
-    const handleLogoClick = () => {
-        setActiveSection(0);
-        // set the active section to -1 to indicate that the logo was clicked
-    }
+  };
 
-    return (
-        <div>
-            <nav className='navbar'>
-                 <a className="navbar-logo" onClick={() => handleLogoClick()}><img src={logo} className="logo" alt="ETER logo"/></a>
-                <ul>
-                    <li className={activeSection === -1 ? 'active' : ''} onClick={() => handleSectionClick(-1)}>ABOUT US</li>
-                    <li className={activeSection === 1 ? 'active' : ''} onClick={() => handleSectionClick(1)}>HOW WE WORK</li>
-                    <li className={activeSection === 2 ? 'active' : ''} onClick={() => handleSectionClick(2)}>CONTACT</li>
-                    <li className={activeSection === 3 ? 'active' : ''} onClick={() => handleSectionClick(3)}>PROJECTS</li>
-                    <li className={activeSection === 3 ? 'active' : ''} onClick={() => handleSectionClick(4)}>PRESS</li>
-                </ul>
+  const handleLogoClick = () => {
+    setActiveSection(0);
+    // set the active section to -1 to indicate that the logo was clicked
 
-            </nav>
-            {activeSection === -1 && <About />}
-            {activeSection === 0 && <Home />}
-            {activeSection === 1 && <Work />}
-            {activeSection === 2 && <Contact />}
-            {activeSection === 3 && <Projects />}
-            {activeSection === 4 && <Press />}
-        </div>
-    );
+    // Set the default background image here if desired
+    document.body.style.backgroundImage = "url('/src/assets/images/default-background.jpg')";
+  };
+
+  return (
+    <div>
+      <nav className='navbar'>
+        <a className={activeSection === 0 ? 'active' : ''} onClick={() => handleSectionClick(0)}><img src={logo} className="logo" alt="ETER logo" /></a>
+        <ul>
+          <li className={activeSection === -1 ? 'active' : ''} onClick={() => handleSectionClick(-1)}>ABOUT US</li>
+          <li className={activeSection === 1 ? 'active' : ''} onClick={() => handleSectionClick(1)}>HOW WE WORK</li>
+          <li className={activeSection === 2 ? 'active' : ''} onClick={() => handleSectionClick(2)}>CONTACT</li>
+          <li className={activeSection === 3 ? 'active' : ''} onClick={() => handleSectionClick(3)}>PROJECTS</li>
+          <li className={activeSection === 4 ? 'active' : ''} onClick={() => handleSectionClick(4)}>PRESS</li>
+        </ul>
+      </nav>
+      {activeSection === -1 && <About />}
+      {activeSection === 0 && <Home />}
+      {activeSection === 1 && <Work />}
+      {activeSection === 2 && <Contact />}
+      {activeSection === 3 && <Projects />}
+      {activeSection === 4 && <Press />}
+    </div>
+  );
 }
 
-
-export default Navbar
+export default Navbar;
--- src/components/Press.jsx
+++ src/components/Press.jsx
@@ -1,16 +1,14 @@
-import React from "react"
-
 
 function Press() {
     return (<div>
 
       
         <div className="press-container">
-        <div class="image-press1"></div>
-        <div class="image-press2"></div>
-        <div class="image-press3"></div>
-        <div class="image-press4"></div>
-        <div class="image-press5"></div>
+        <div className="image-press1"></div>
+        <div className="image-press2"></div>
+        <div className="image-press3"></div>
+        <div className="image-press4"></div>
+        <div className="image-press5"></div>
 </div>
 
     </div>)
